a99453d05af033064c6fd535ec3a6e176619ae03
Sally-Builds <uzoagulujoshua@yahoo.com>
2022-12-13 23:57:01 +0100

    update readme

---
+++ copy.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+const path = require('path')
+
+// fs.copyFile(path.join(__dirname, '/build/contracts/futfund.json'), path.join(__dirname, '/client/src/futfund.json'), (err) => {
+//     if (err) throw err;
+//     console.log('File was copied to destination');
+//   });
