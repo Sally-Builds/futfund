a8cfcaf2eab39fcfba10622f03861dfa949ab184
Sally-Builds <uzoagulujoshua@yahoo.com>
2022-12-13 23:54:07 +0100

    test for contract futfund

--- test/futfund.test.js
+++ test/futfund.test.js
@@ -1,16 +1,17 @@
-const Base = artifacts.require("base")
+// const Base = artifacts.require("base")
+const futfund = artifacts.require("futfund.sol")
 
 
-contract('Base', (accounts) => {
+contract('futfund', (accounts) => {
     before(async () => {
-        base = await Base.deployed()
+        base = await futfund.deployed()
     })
 
     it("Ensure the project was created successfully", async () => {
         const nameOfProject = "Toilet Rebuilding"
         const expectedAmt = 25 
         const startDate = 2
-        const endDate = '8y'
+        const endDate = 5
 
         let newProject = await base.createProject(nameOfProject, expectedAmt, startDate, endDate)
         let projects = await base.getProjects()
